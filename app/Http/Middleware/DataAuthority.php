88c47329124870bc13a86f2d887a3a698741e447
songxi <476396956@qq.com>
2018-11-26 15:24:06 +0800

    2.2 数据权限 -------------------------- 修改 middleware DataAuthority，连接匹配。

--- app/Http/Middleware/DataAuthority.php
+++ app/Http/Middleware/DataAuthority.php
@@ -18,20 +18,17 @@ class DataAuthority
     public function handle($request, Closure $next,$model)
     {
         $path = $request->path();
-        if(preg_match('/admin\/'.strtolower($model).'\/(\d+)\/edit/',$path) > 0 ||
-            preg_match('/admin\/'.strtolower($model).'\/(\d+)/',$path) > 0){
 
-            if(preg_match('(\d+)',$path,$result) > 0){
-                if(is_array($result)&&!empty($result)&&!empty($result[0])){
-                    $id =  $result[0];
-                    $admin_ids = DB::select('select admin_id from '.$model.' where id = ?', [$id]);
-                    if(is_array($admin_ids)&&!empty($admin_ids)&&!empty($admin_ids[0])){
-                        $admin_id = $admin_ids[0]->admin_id;
-                        $admin_user = Admin::user();
-                        if($admin_id != $admin_user->id &&
-                            !$admin_user->isRole('datamanager')){
-                            return redirect()->to('admin/deny');
-                        }
+        if(preg_match('(\d+)',$path,$result) > 0){
+            if(is_array($result)&&!empty($result)&&!empty($result[0])){
+                $id =  $result[0];
+                $admin_ids = DB::select('select admin_id from '.$model.' where id = ?', [$id]);
+                if(is_array($admin_ids)&&!empty($admin_ids)&&!empty($admin_ids[0])){
+                    $admin_id = $admin_ids[0]->admin_id;
+                    $admin_user = Admin::user();
+                    if($admin_id != $admin_user->id &&
+                        !$admin_user->isRole('datamanager')){
+                        return redirect()->to('admin/deny');
                     }
                 }
             }
